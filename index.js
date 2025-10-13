4d828c4cb0c811362afcc31e94a773cafae914cc
ansh <anshdewangna52@gmail.com>
2025-10-13 15:45:31 +0530

    change in serverless index.js

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './DB/connectDB.js';
-import authRoutes from './routes/authRoutes.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import { connectDB } from "../DB/connectDB.js";
+import authRoutes from "../routes/authRoutes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import serverless from "serverless-http";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json());//to allow the parse incoming request with JSON data(reuqest.body) 
-app.use(cookieParser());// allow us to parse incoming cookies
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth",authRoutes);
+connectDB();
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log('Server is running on port:', PORT);
-});
+export const handler = serverless(app);
+
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import { connectDB } from './DB/connectDB.js';
+// import authRoutes from './routes/authRoutes.js';
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(cors({origin:"http://localhost:5173",credentials:true}));
+
+// app.use(express.json());//to allow the parse incoming request with JSON data(reuqest.body) 
+// app.use(cookieParser());// allow us to parse incoming cookies
+
+// app.use("/api/auth",authRoutes);
+
+// app.listen(PORT, () => {
+//     connectDB();
+//     console.log('Server is running on port:', PORT);
+// });
