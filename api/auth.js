5cb875d5bf1e6f6041d9408c4bd393c16c02266a
coderwithansh <anshdewangan52@gmail.com>
2025-10-30 10:24:54 +0530

    Initialize authentication API with Express and MongoDB

---
+++ api/auth.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "../routes/authRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// --- CORS ---
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://authentication-frontend-lac.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
+// --- Middlewares ---
+app.use(express.json());
+app.use(cookieParser());
+
+// --- Mongo connection cached ---
+let conn = null;
+async function connectDB() {
+  if (conn) return;
+  conn = await mongoose.connect(process.env.MONGO_URL);
+  console.log("✅ MongoDB connected");
+}
+connectDB();
+
+// --- Routes ---
+app.use("/api/auth", router);
+
+// ✅ Export as serverless function
+export default app;
