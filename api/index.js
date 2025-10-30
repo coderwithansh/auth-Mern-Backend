2793e194d36855c6a13db4f3637db0628bf0b2a3
ansh <anshdewangna52@gmail.com>
2025-10-30 10:20:16 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -3,56 +3,37 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import router from "./routes/authRoutes.js";
+import router from "../routes/authRoutes.js"; 
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-let isConnected = false; // connection cache
-
-const connectDB = async () => {
-  if (isConnected) {
-    console.log("✅ MongoDB already connected");
-    return;
-  }
-
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = conn.connections[0].readyState;
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
-    process.exit(1);
-  }
-};
 const app = express();
 
-// Initialize Express app
-// middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://authentication-frontend-lac.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 app.use(express.json());
-app.use(cors({ origin: "*" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use(async (req, res, next) => {
-  if (isConnected) {
-    await connectDB();
-    console.log("✅ MongoDB already connected");
-  }
-  next();
-});
-//routes
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URL);
+  isConnected = true;
+}
+connectDB();
+
 app.use("/api/auth", router);
 
-// Error handler
-app.use((err, req, res, next) => {
-  console.error("Error:", err);
-  res.status(500).json({ error: err.message || "Internal Server Error" });
-});
 export default app;
 
 // import express from 'express';
